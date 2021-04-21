3cff66591bf102e803e7094d9d599ef8636a8e60
Tejash Datta <tejashdatt@gmail.com>
2021-04-21 22:57:53 +0530

    simplification

--- http_client.rb
+++ http_client.rb
@@ -8,9 +8,13 @@ class HttpClient
     @parameters = parameters
     @number_of_threads = number_of_threads.to_i
     @number_of_loops = number_of_loops.to_i
+    @response_codes = Hash.new(0)
+    @response_bodies = []
   end
 
   def run(output_type)
+    send_requests_concurrently_in_loops
+
     case output_type
     when 'response-codes-aggregation'
       display_response_codes_aggregation
@@ -21,33 +25,34 @@ class HttpClient
     end
   end
 
+  private
+
   def display_response_codes_aggregation
-    response_codes = Hash.new(0)
-    response_codes_semaphore = Mutex.new
-
-    send_requests_in_concurrent_loops do |response|
-      response_codes_semaphore.synchronize { response_codes[response.code] += 1 }
-    end
-
-    response_codes.each { |code, count| puts "#{code}: #{count}" }
+    @response_codes.each { |response_code, count| puts "#{response_code}: #{count}" }
   end
 
   def display_response_bodies
-    send_requests_in_concurrent_loops { |response| puts response.body }
+    @response_bodies.each { |response_body| puts response_body }
   end
 
-  private
+  def send_requests_concurrently_in_loops
+    save_response_semaphore = Mutex.new
 
-  def send_requests_in_concurrent_loops
-    threads = []
-    @number_of_threads.times do
-      threads << Thread.new do
-        @number_of_loops.times do
-          yield HttpRequest.new(@url, @method, @parameters).send_request
+    @number_of_loops.times do
+      threads = []
+      @number_of_threads.times do
+        threads << Thread.new do
+          response = HttpRequest.new(@url, @method, @parameters).send_request
+          save_response_semaphore.synchronize { save_response_code_and_body(response) }
         end
       end
+      threads.each(&:join)
     end
-    threads.each(&:join)
+  end
+
+  def save_response_code_and_body(response)
+    @response_bodies << response.body
+    @response_codes[response.code] += 1
   end
 end
 
