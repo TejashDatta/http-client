e60bc65544c44aa0d1826105d7376f7bdd93bea5
Tejash Datta <tejashdatt@gmail.com>
2021-04-26 09:17:46 +0530

    dry HttpClient instances

--- spec/http_client_spec.rb
+++ spec/http_client_spec.rb
@@ -7,48 +7,51 @@ NUMBER_OF_LOOPS = 10
 output_count = NUMBER_OF_THREADS * NUMBER_OF_LOOPS
 
 describe 'HttpClient' do
+  let(:http_client) {
+    HttpClient.new(URL, '', QUERY_STRING, NUMBER_OF_THREADS, NUMBER_OF_LOOPS, '')
+  }
+
   describe '#run' do
     context 'when method is get and output_type is response-bodies' do
+      before do 
+        http_client.instance_variable_set(:@method, 'get')
+        http_client.instance_variable_set(:@output_type, 'response-bodies')
+      end
+
       it 'displays all response-bodies' do
-        expect {
-          HttpClient
-            .new(URL, 'get', QUERY_STRING, NUMBER_OF_THREADS, NUMBER_OF_LOOPS, 'response-bodies')
-            .run
-        }.to output(HTML_RESPONSE_BODY * output_count).to_stdout
+        expect { http_client.run }.to output(HTML_RESPONSE_BODY * output_count).to_stdout
       end
     end
 
     context 'when method is post and output_type is response-codes-aggregation' do
-      it 'displays response code aggregation for all codes 200' do
-        expect {
-          HttpClient
-            .new(URL, 'post', QUERY_STRING, NUMBER_OF_THREADS, NUMBER_OF_LOOPS, 'response-codes-aggregation')
-            .run 
-        }.to output("200: #{output_count}\n").to_stdout
+      before do
+        http_client.instance_variable_set(:@method, 'post')
+        http_client.instance_variable_set(:@output_type, 'response-codes-aggregation')
+      end
+
+      it 'displays response codes aggregation for all codes 200' do
+        expect { http_client.run }.to output("200: #{output_count}\n").to_stdout
       end
     end
   end
 
+  shared_context 'http_client has received get responses' do
+    before do
+      http_client.instance_variable_set(:@method, 'get')
+      http_client.send(:send_requests_concurrently_in_loops)
+    end
+  end
+
   describe '#send_requests_concurrently_in_loops' do
-    let(:http_client) {
-      HttpClient.new(URL, 'get', QUERY_STRING, NUMBER_OF_THREADS, NUMBER_OF_LOOPS, '') 
-    }
-    before { http_client.send(:send_requests_concurrently_in_loops) }
-    
-    it 'receives all responses' do
+    include_context 'http_client has received get responses'
+
+    it 'has received all responses' do
       expect(http_client.instance_variable_get(:@responses).count).to eq(output_count)
     end
   end
 
-  shared_context 'uses http_client that has received get responses' do
-    let(:http_client) {
-      HttpClient.new(URL, 'get', QUERY_STRING, NUMBER_OF_THREADS, NUMBER_OF_LOOPS, '')
-    }
-    before { http_client.send(:send_requests_concurrently_in_loops) }
-  end
-
   describe '#display' do
-    include_context 'uses http_client that has received get responses'
+    include_context 'http_client has received get responses'
 
     context 'when output_type is response-codes-aggregation' do
       before { http_client.instance_variable_set(:@output_type, 'response-codes-aggregation') }
@@ -76,7 +79,7 @@ describe 'HttpClient' do
   end
 
   describe '#aggregate_response_codes' do
-    include_context 'uses http_client that has received get responses'
+    include_context 'http_client has received get responses'
     
     it 'aggregates response codes that are all 200' do
       expect(http_client.send(:aggregate_response_codes)).to eq({ '200' => output_count })
