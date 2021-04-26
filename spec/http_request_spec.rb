518252e9b4b98266478d8a16782625cb8c04a3bf
Tejash Datta <tejashdatt@gmail.com>
2021-04-26 13:29:14 +0530

    dry http request objects

--- spec/http_request_spec.rb
+++ spec/http_request_spec.rb
@@ -2,36 +2,33 @@ require_relative '../http_request'
 require_relative 'constants'
 
 describe 'HttpRequest' do
-  describe '#parse_parameters_to_hash' do
-    it 'parses correctly' do
-      expect(HttpRequest.new('', '', '').send(:parse_parameters_to_hash, 'a =abc efg, b= 20,c=3')).to eq(
-        { 'a' => 'abc efg', 'b' => '20', 'c' => '3' }
-      )
-    end
-  end
+  let(:http_request_get) { HttpRequest.new(URL, 'get', '') }
+  let(:http_request_get_with_query) { HttpRequest.new(URL, 'get', QUERY_STRING) }
+  let(:http_request_post) { HttpRequest.new(URL, 'post', '') }
+  let(:http_request_get_with_query) { HttpRequest.new(URL, 'post', QUERY_STRING) }
 
   describe '#send_request' do
     context 'when get request without query parameters' do
       it 'has response with status code 200' do
-        expect(HttpRequest.new(URL, 'get', '').send_request.code).to eq('200')
+        expect(http_request_get.send_request.code).to eq('200')
       end
     end
 
     context 'when get request with query parameters' do
       it 'has response with status code 200' do
-        expect(HttpRequest.new(URL, 'get', QUERY_STRING).send_request.code).to eq('200')
+        expect(http_request_get_with_query.send_request.code).to eq('200')
       end
     end
 
     context 'when post request without post parameters' do
       it 'has response with status code 200' do
-        expect(HttpRequest.new(URL, 'post', '').send_request.code).to eq('200')
+        expect(http_request_post.send_request.code).to eq('200')
       end
     end
 
     context 'when post request with post parameters' do
       it 'has response with status code 200' do
-        expect(HttpRequest.new(URL, 'post', QUERY_STRING).send_request.code).to eq('200')
+        expect(http_request_get_with_query.send_request.code).to eq('200')
       end
     end
 
@@ -43,17 +40,25 @@ describe 'HttpRequest' do
       end
     end
   end
+
+  describe '#parse_parameters_to_hash' do
+    it 'parses correctly' do
+      expect(http_request_get_with_query.send(:parse_parameters_to_hash, 'a =abc efg, b= 20,c=3')).to eq(
+        { 'a' => 'abc efg', 'b' => '20', 'c' => '3' }
+      )
+    end
+  end
   
   describe '#get' do
     context 'when without query parameters' do
       it 'has response with status code 200' do
-        expect(HttpRequest.new(URL, 'get', '').send(:get).code).to eq('200')
+        expect(http_request_get.send(:get).code).to eq('200')
       end
     end
 
     context 'when with query parameters' do
       it 'has response with status code 200' do
-        expect(HttpRequest.new(URL, 'get', QUERY_STRING).send(:get).code).to eq('200')
+        expect(http_request_get_with_query.send(:get).code).to eq('200')
       end
     end
   end
@@ -61,13 +66,13 @@ describe 'HttpRequest' do
   describe '#post' do
     context 'when without query parameters' do
       it 'has response with status code 200' do
-        expect(HttpRequest.new(URL, 'post', '').send(:post).code).to eq('200')
+        expect(http_request_post.send(:post).code).to eq('200')
       end
     end
 
     context 'when with query parameters' do
       it 'has response with status code 200' do
-        expect(HttpRequest.new(URL, 'post', QUERY_STRING).send(:post).code).to eq('200')
+        expect(http_request_get_with_query.send(:post).code).to eq('200')
       end
     end
   end
